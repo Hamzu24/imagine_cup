1fbd6952c564b148ccf0eb94a4bb140701b1fe90
Hamza Pereira <adj-obj@protonmail.com>
2024-10-01 13:43:57 -0400

    print test

--- csi_test1.py
+++ csi_test1.py
@@ -1,1 +1,11 @@
-from CSIKit.reader import 
+import numpy as np
+from CSIKit.reader import get_reader
+from CSIKit.util import csitools
+
+file_path = 'example_csi_data/40mhz_1600085286.pcap'
+reader = get_reader(file_path)
+csi_data = reader.read_file(file_path, scaled=True)
+csi_matrix, no_frames, no_subcarriers = csitools.get_CSI(csi_data)
+print(csi_matrix)
+print(no_frames)
+print(no_subcarriers)
